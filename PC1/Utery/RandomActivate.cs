e07a6cd95dac20f07b5f582c66177f5c6be33b29
DilnyKyberna <dilnykyberna2020@gmail.com>
2020-07-12 15:09:11 +0200

    PC1

---
+++ PC1/Utery/RandomActivate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomActivate : MonoBehaviour
+{
+    public List<GameObject> gameObjects;
+
+    bool _inUse = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Randomize();
+    }
+
+    public void Randomize()
+    {
+        StartCoroutine(RandomizeEnumerator());
+    }
+
+    IEnumerator RandomizeEnumerator()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (!_inUse)
+        {
+            _inUse = true;
+
+            foreach (var item in gameObjects)
+            {
+                item.SetActive(false);
+            }
+            gameObjects[Random.Range(0, gameObjects.Count)].SetActive(true);
+
+            _inUse = false;
+        }
+    }
+}
---
+++ PC1/Utery/mckyberna/Assets/McTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class McTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEvent TouchAction;
+
+    //public McTimer Timer;
+    public McFood Food;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        this.TouchAction?.Invoke();
+
+        GameObject.FindGameObjectWithTag("Player").GetComponent<McTimer>().StartTimer(this.Food);
+        //this.Timer.StartTimer(this.Food);
+        this.Food.SetFood();
+    }
+}
