86bdd1975af225f59fe4dc8d156ed37e25440e67
DilnyKyberna <dilnykyberna2020@gmail.com>
2020-07-12 15:25:50 +0200

    PC8

---
+++ PC8/mckyberna/Assets/McAdditionScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class McAdditionScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddPoints(float value)
+    {
+        var label = this.GetComponent<Text>();
+
+        float score = 0;
+        float.TryParse(label.text, out score);
+
+        score += value;
+        label.text = $"{score.ToString("0.00")}";
+    }
+}
---
+++ PC8/mckyberna/Assets/TurboScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurboScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode Key;
+    public Rigidbody Car;
+    public float Strength = 6f;
+
+    public Camera Camera;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.Camera != null)
+        {
+            this.Camera.fieldOfView = Mathf.Clamp(60.0f + this.Car.velocity.magnitude, 45f, 90f);
+        }
+
+        if (Input.GetKey(this.Key))
+        {
+            this.Car.AddForce(this.Car.gameObject.transform.forward * this.Strength, ForceMode.VelocityChange);
+        }
+    }
+}
