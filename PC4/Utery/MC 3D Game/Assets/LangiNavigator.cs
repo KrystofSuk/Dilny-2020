f169cd3a63c367a7944866d1d73cbfa3f9b3c68b
DilnyKyberna <dilnykyberna2020@gmail.com>
2020-07-12 15:15:14 +0200

    PC4

---
+++ PC4/Utery/MC 3D Game/Assets/LangiNavigator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LangiNavigator : MonoBehaviour
+{
+    // Start is called before the first frame u
+    public bool LockY = true;
+    private GameObject[] finishObjects;
+
+    public McFood Food;
+    private bool toFood = true;
+
+    void Start()
+    {
+        this.finishObjects = GameObject.FindGameObjectsWithTag("Finish");
+
+        Food.FoodSet.AddListener(() => toFood = false);
+        Food.FoodRemoved.AddListener(() => toFood = true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Vector3.zero;
+        var minDist = float.MaxValue;
+
+        if (toFood)
+        {
+            foreach (var pickup in GameObject.FindGameObjectsWithTag("Pickup"))
+            {
+                var distvec = this.gameObject.transform.position - pickup.transform.position;
+                if (distvec.magnitude < minDist)
+                    dir = distvec.normalized;
+            }
+        }
+        else
+        {
+            foreach (var finishobject in finishObjects)
+            {
+                var distvec = this.gameObject.transform.position - finishobject.transform.position;
+                if (distvec.magnitude < minDist)
+                    dir = distvec.normalized;
+            }
+        }
+
+
+
+        if (this.LockY)
+        {
+            dir.y = 0;
+            dir.Normalize();
+        }
+
+
+        this.transform.rotation = Quaternion.LookRotation(-dir);
+    }
+}
---
+++ PC4/Utery/MC 3D Game/Assets/McDelivery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class McDelivery : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<McFood> Food = new List<McFood>();
+    public Text ScoreLabel;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        float add = 0;
+        foreach (var f in this.Food)
+        {
+            if (!f.HasFood()) continue;
+
+            //Destroy(f);
+
+            f.RemoveFood();
+            add += f.Value;
+        }
+
+
+
+        int score = 0;
+        int.TryParse(this.ScoreLabel.text, out score);
+
+        this.ScoreLabel.text = $"{score + add}";
+
+        GameObject.FindGameObjectWithTag("Player")?.GetComponent<McTimer>()?.StopTimer();
+    }
+}
