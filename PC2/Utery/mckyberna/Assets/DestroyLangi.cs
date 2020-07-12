1db936e89609a4c3b01399f73fa430650d89a86b
DilnyKyberna <67861339+DilnyKyberna@users.noreply.github.com>
2020-07-12 15:10:48 +0200

    PC2

---
+++ PC2/Utery/mckyberna/Assets/DestroyLangi.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyLangi : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string EndScene;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var langi = GameObject.FindGameObjectWithTag("Player");
+
+        if (this.EndScene != null)
+            SceneManager.LoadScene(this.EndScene);
+
+        if (langi != null)
+            Destroy(langi);
+    }
+}
---
+++ PC2/Utery/mckyberna/Assets/LangiJump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LangiJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode Key;
+    public Rigidbody Car;
+    public float Force = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKeyDown(this.Key))
+            return;
+
+        this.Car.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
+    }
+}
