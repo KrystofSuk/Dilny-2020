14f910211a403e8713ab8dbc2596824de92543f8
DilnyKyberna <dilnykyberna2020@gmail.com>
2020-07-12 15:24:37 +0200

    PC9

---
+++ PC9/Pondeli/Pong/Assets/Prefabs/RandomActivate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomActivate : MonoBehaviour
+{
+    public List<GameObject> gameObjects;
+
+    bool _inUse = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Randomize();
+    }
+
+    public void Randomize()
+    {
+        StartCoroutine(RandomizeEnumerator());
+    }
+
+    IEnumerator RandomizeEnumerator()
+    {
+Debug.Log("Call");
+        yield return new WaitForSeconds(0.1f);
+        if (!_inUse)
+        {
+            _inUse = true;
+Debug.Log("DDD");
+
+            foreach (var item in gameObjects)
+            {
+                item.SetActive(false);
+            }
+            gameObjects[Random.Range(0, gameObjects.Count)].SetActive(true);
+
+Debug.Log("aaa");
+            _inUse = false;
+        }
+    }
+}
---
+++ PC9/Utery/New Unity Project/Assets/LoadLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string LevelName;
+
+    public void Load(){
+        Application.LoadLevel(LevelName);
+    }
+    
+    public void Quit(){
+        Application.Quit();
+    }
+}
